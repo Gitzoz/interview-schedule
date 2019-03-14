3b3a49ea4dc5df9d5dc17a5adebebbaf63acf3fc
Stefan Röhrbein <stefan.roehrbein@kaufhof.de>
2019-03-14 10:35:17 +0100

    Adding model classes

--- interview_schedule/api/models.py
+++ interview_schedule/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField('user created at')
+
+class Candidate(User):
+    pass
+
+class Interviewer(User):
+    pass
+
+
+class AppointmentSlot(models.Model):
+    begin = models.DateTimeField('appointment start')
+    end = models.DateTimeField('appointment end')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
